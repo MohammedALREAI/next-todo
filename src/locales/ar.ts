2224441d66a2c576ee99d683901d8ef85299ca91
Mohammed alreai <mhamad.aa1997.aa@gmail.com>
2021-12-14 11:15:19 +0200

    finsh the translate

---
+++ src/locales/ar.ts
@@ -0,0 +1,14 @@
+export  const Ar={
+      
+    locale: "Ar",
+    language: "العربية",
+    todo: "تودوا",
+    placeholder: "اضافة  تاسك  جديدة",
+    addButton: "اضافة ",
+    left: "المتبقية ",
+    task:"تاسك",
+    tasks:"تاسكات",
+    followers: "متابعين",
+    "go back":"الرجوع  االي  الوراء"
+
+}
---
+++ src/locales/en.ts
@@ -0,0 +1,12 @@
+ export  const En={
+      
+        locale: "en",
+        language: "English",
+        todo: "todo",
+        placeholder: "Add  a  new  take  here",
+        addButton: "add",
+        left: "left",
+        followers: "followers",
+        task:"task",
+        "go back":"Go  Back"
+  }
